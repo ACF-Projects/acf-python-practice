110111230351bdd1ed1736ceec88404a11c7b44d
LWCoding <DiamondMob01@gmail.com>
2023-12-22 14:46:53 -0800

    Added solutions for database-scanner assignment

---
+++ database-scanning/complete/main.py
@@ -0,0 +1,55 @@
+# DATABASE SCANNING
+
+# The following includes our database list. 
+# You do NOT need to modify it in any way 
+# for this exercise.
+# It contains lists inside of it, each of 
+# which follow the same syntax:
+# [ name, title, salary ]
+database = [["Kevin", "CEO", 137500],
+           ["Tracy", "Secretary", 78000],
+           ["Bob", "Worker", 32000],
+           ["Pete", "Worker", 27500],
+           ["Gavin", "Worker", 26000],
+           ["Mildew", "Worker", 31000],
+           ["Lucas", "Worker", 23500],
+           ["Ashley", "Worker", 46000],
+           ["Eric", "Worker", 32000],
+           ["Ariel", "Worker", 39000],
+           ["Chris", "Worker", 26000],
+           ["Catherine", "Worker", 31000],
+           ["Jacy", "Worker", 23500]]
+
+# PART 1: Print the information of every 
+# employee in the database on separate lines. 
+# (Do not print the database list, but print 
+# every list inside.)
+
+for worker in database:
+    print(worker)
+  
+# PART 2: Print out the NAME of every worker 
+# that has a salary under $30,000.
+# (The expected output should be Pete, Gavin, 
+# Lucas, Chris, and Lucy.)
+
+for employee in database:
+    salary = int(employee[2])
+    if salary < 30000:
+        print(worker[0])
+
+# PART 3: If any employee that is a WORKER in 
+# the database has a salary over $30,000, change 
+# their title from WORKER to OFFICER. Print the 
+# list to make sure your changes are correct.
+# (Bob, Mildew, Ashley, Eric, Ariel, and 
+# Catherine should now appear as "Officer".)
+
+for employee in database:
+    role = employee[1]
+    salary = int(employee[2])
+    if role == "Worker" and salary > 30000:
+        employee[1] = "Officer"
+
+for worker in database:
+    print(worker)
