e41026b05d9394d1c9c8573d51238dd4d488bb5e
LWCoding <DiamondMob01@gmail.com>
2024-01-20 12:54:06 -0800

    Added new visualize_list function to sortfunc for instructor to make assignment more engaging

--- sort-optimization/sortfunc.py
+++ sort-optimization/sortfunc.py
@@ -24,3 +24,23 @@ def tim_sort(list_nums):
     Returns the sorted list.
     """
     return sorted(list_nums)
+
+def visualize_list(list_nums):
+    """
+    Given a list of integers, prints out
+    a representation of the list as a
+    series of vertical bars. Lower bars
+    are lower numbers, and vice versa.
+    Truncates to average number of blocks
+    for low/high nums, so 58 blocks will
+    not be 58 squares; it will be rounded
+    to according to the min/max.
+    """
+    low = min(list_nums)
+    high = max(list_nums)
+    count = len(set(list_nums))
+    print("--- List visualized: ---")
+    for num in list_nums:
+        height = int(count * num / (high - low + 1))
+        print("🟨" * height + "⬜" * (count - height))
+    print("------------------------")
